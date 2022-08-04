63a2b2236ab812510b85b6f665baff80b7eae1e4
Mayer Adelman <mayerad@amazon.com>
2022-08-04 15:06:02 -0700

    merged Daniel's code

--- cdk_framework/EKS/lib/utils/validate-test-case-config.ts
+++ cdk_framework/EKS/lib/utils/validate-test-case-config.ts
@@ -2,28 +2,25 @@ import { ClusterStack } from '../stacks/cluster-stack';
 
 const configKeys = new Set(['cluster_name', 'sample_app_image_uri', 'sample_app_mode', 'collector_config'])
 
-export function validateTestcaseConfig(info: Object, clusterStackMap: Map <string, ClusterStack>){
+export function validateTestcaseConfig(info: any, clusterStackMap: Map <string, ClusterStack>){
     const data = Object(info)
     if (!data['test_case']) {
         throw new Error('No test_case field in the yaml file')
     }
     const testCaseConfig = data['test_case']
     const providedKeys = new Set(Object.keys(testCaseConfig))
-
     // validate that all provided keys are valid
     providedKeys.forEach(key => {
         if (!configKeys.has(key)) {
             throw new Error(`Provided key ${key} is not valid`)
         }
     })
-
     // validate that all required keys are provided
     configKeys.forEach(key => {
         if (!providedKeys.has(key)) {
             throw new Error(`${key} must be specified`)
         }
     })
-
     // validate each key-value pair
     for (const [key, value] of Object.entries(testCaseConfig)) {
         validateValue(key, value, clusterStackMap)
